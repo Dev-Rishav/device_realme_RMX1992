2b9dedcbaebbe5090e90b43b65ffe8ae40301629
DaddyRishav <rishavchakraborty94@gmail.com>
2020-07-03 05:34:07 +0530

    x2: Build with GAPPs

--- derp_x2.mk
+++ derp_x2.mk
@@ -9,6 +9,13 @@ $(call inherit-product, device/realme/x2/device.mk)
 # Inherit some common Derpfest stuff.
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
+#GAPPS
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+TARGET_MINIMAL_APPS := false
+TARGET_INCLUDE_STOCK_ARCORE := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_x2
 PRODUCT_DEVICE := x2
